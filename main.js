26415433970cf727193ef4a5c2ca2d21d2efb28e
AaryanShah14 <83456072+AaryanShah14@users.noreply.github.com>
2021-09-19 12:05:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+Status="";
+img="";
+objects=[];
+
+function setup(){
+    canvas=createCanvas(380, 380);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+    
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Baby";
+}
+
+function draw(){
+    image(video, 0,0, 380,380);
+
+    if(Status!=""){
+        r=random(255);
+        g=random(255);
+        b=random(255);
+        objectDetector.detect(video, gotResult);
+         for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status : Baby Detected";
+            document.getElementById("number_of_objects").innerHTML="Number of Objects: " + objects.length;
+            fill(r,g,b);
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+ percent+ "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke(r,g,b);  
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            
+         }
+    }
+}
+
+function modelLoaded(){
+    console.log("Model Is Loaded");
+    Status=true;
+
+
+}
+
+function gotResult(error,results){
+if(error){
+    console.error(error);
+}   
+else{
+    console.log(results);
+    objects=results;
+}
+}
